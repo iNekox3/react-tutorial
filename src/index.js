85f0ad3646a78c60343a9dea700908d61139c715
iNekox3 <inekox3@hotmail.com>
2018-05-18 19:43:04 +0800

    index.js: change values in board from numbers to letters

--- src/index.js
+++ src/index.js
@@ -24,19 +24,19 @@ class Board extends React.Component {
       <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare('C')}
+          {this.renderSquare('A')}
+          {this.renderSquare('T')}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare('M')}
+          {this.renderSquare('E')}
+          {this.renderSquare('W')}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare('P')}
+          {this.renderSquare('I')}
+          {this.renderSquare('E')}
         </div>
       </div>
     );
